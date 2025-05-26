b1bceb32bbf84015d853d90d7d3d63a2c277b86b
AiMIC_DEVAiNCE <realktemgraphics9@gmail.com>
2025-05-26 18:25:22 +0800

    v9

--- app/src/main/java/aimee/protoapps/com/a112823/MainActivity.java
+++ app/src/main/java/aimee/protoapps/com/a112823/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -15,16 +14,17 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.JavascriptInterface;
 import android.support.v7.app.AppCompatActivity;
-import androidx.annotation.Nullable;
 import android.widget.Toast;
 
-import java.util.Calendar;
-import android.app.AlertDialog;
+import androidx.annotation.Nullable;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
 
+import android.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -36,26 +36,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the WebView
         webView = findViewById(R.id.webView);
 
-        // Enable debugging for WebView
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WebView.setWebContentsDebuggingEnabled(true);
         }
 
-        // Enable JavaScript and other settings
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true); // Enable DOM storage
+        webSettings.setDomStorageEnabled(true);
 
-        // Set a WebViewClient to handle loading URLs
         webView.setWebViewClient(new WebViewClient());
-
-        // Enable JavaScript interface to communicate between Java and JavaScript
         webView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
 
-        // Handle file input for image upload
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
@@ -64,10 +57,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 MainActivity.this.filePathCallback = filePathCallback;
 
-                // Create an Intent to allow selection of all image file types
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");  // General wildcard for images
+                intent.setType("image/*");
 
                 String[] mimeTypes = {"image/jpeg", "image/png", "image/gif", "image/webp", "image/bmp", "image/svg+xml", "image/tiff"};
                 intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
@@ -77,13 +69,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Load the HTML file from the assets folder
         webView.loadUrl("file:///android_asset/index.html");
+
         checkForUpdate();
-
-
-
-        // Schedule notifications
         scheduleDailyNotification();
         scheduleMonthlyNotification();
     }
@@ -96,12 +84,10 @@ public class MainActivity extends AppCompatActivity {
             if (filePathCallback == null) return;
             Uri[] results = null;
 
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    String dataString = data.getDataString();
-                    if (dataString != null) {
-                        results = new Uri[]{Uri.parse(dataString)};
-                    }
+            if (resultCode == RESULT_OK && data != null) {
+                String dataString = data.getDataString();
+                if (dataString != null) {
+                    results = new Uri[]{Uri.parse(dataString)};
                 }
             }
             filePathCallback.onReceiveValue(results);
@@ -109,14 +95,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Schedule daily notification at 8:30 PM
     private void scheduleDailyNotification() {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 20); // 8 PM
-        calendar.set(Calendar.MINUTE, 00); // 00 minutes
-        calendar.set(Calendar.SECOND, 0); // 0 seconds
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
 
-        // If the time is in the past, schedule for tomorrow
         if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
             calendar.add(Calendar.DAY_OF_YEAR, 1);
         }
@@ -129,22 +113,17 @@ public class MainActivity extends AppCompatActivity {
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         if (alarmManager != null) {
-            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                    calendar.getTimeInMillis(),
-                    AlarmManager.INTERVAL_DAY,
-                    pendingIntent);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
         }
     }
 
-    // Schedule monthly notification on the 28th at 6:30 AM
     private void scheduleMonthlyNotification() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.DAY_OF_MONTH, 28);
-        calendar.set(Calendar.HOUR_OF_DAY, 6); // 6 AM
-        calendar.set(Calendar.MINUTE, 30); // 30 minutes
-        calendar.set(Calendar.SECOND, 0); // 0 seconds
+        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
 
-        // If the date is in the past, schedule for next month
         if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
             calendar.add(Calendar.MONTH, 1);
         }
@@ -157,28 +136,23 @@ public class MainActivity extends AppCompatActivity {
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         if (alarmManager != null) {
-            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                    calendar.getTimeInMillis(),
-                    AlarmManager.INTERVAL_DAY * 30,
-                    pendingIntent);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY * 30, pendingIntent);
         }
     }
 
-    // Static inner class for JavaScript to call Java methods
     public static class JavaScriptInterface {
         Context mContext;
 
-        /** Instantiate the interface and set the context */
         JavaScriptInterface(Context c) {
             mContext = c;
         }
 
-        // Example method that can be called from JavaScript
         @JavascriptInterface
         public void showToast(String message) {
             Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
         }
     }
+
     private void checkForUpdate() {
         new Thread(() -> {
             try {
@@ -199,13 +173,13 @@ public class MainActivity extends AppCompatActivity {
                 String latestVersion = json.split("\"tag_name\":\"")[1].split("\"")[0];
                 String releaseURL = json.split("\"html_url\":\"")[1].split("\"")[0];
 
-                String currentVersion = "9.0"; // Your current version
+                String currentVersion = "9.0"; // Make sure this matches your latest version
 
                 if (!latestVersion.equals(currentVersion)) {
                     runOnUiThread(() -> {
                         new AlertDialog.Builder(MainActivity.this)
-                                .setTitle("112823 Update Available")
-                                .setMessage("112823 " + latestVersion + " is available mahal kaya update mo na please! HAHAHA")
+                                .setTitle("New Update Available")
+                                .setMessage("ProtoView " + latestVersion + " is out now!")
                                 .setPositiveButton("Update", (dialog, which) -> {
                                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(releaseURL)));
                                 })
@@ -219,6 +193,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }
+
     @Override
     public void onBackPressed() {
         if (webView != null && webView.canGoBack()) {
